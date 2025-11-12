13db67b0a4f2c3610c3659ed6517b67c24f293f4
DEVducanh <nguyenanh191025@gmail.com>
2025-11-12 19:02:56 +0700

    deploy

--- src/components/AppRoutes/index.jsx
+++ src/components/AppRoutes/index.jsx
@@ -1,5 +1,3 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
-
 import AdminLayout from '@/layouts/AdminLayout'
 import DefaultLayout from '@/layouts/DefaultLayout'
 
@@ -28,6 +26,7 @@ import StaffManagement from '@/pages/admin/StaffManagement'
 import UserManagement from '@/pages/admin/UserManagement'
 import ContactPage from '@/pages/client/Contact/ContactPage'
 import PaymentResult from '../PaymentResult'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 const AppRoutes = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,22 @@ import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ConfigProvider } from 'antd'
-import { BrowserRouter } from 'react-router'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: '#ff6900',
-          },
-        }}
-      >
-        <StrictMode>
-          <App />
-        </StrictMode>
-      </ConfigProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#ff6900',
+        },
+      }}
+    >
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </ConfigProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 )
